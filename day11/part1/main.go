7eaa2c1091eef511aacc2812ec348632752b8bd7
Thanoj Singh <thanoj.singh@gmail.com>
2025-12-26 14:50:27 -0500

    day 11 part 1

---
+++ day11/part1/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	b, err := os.ReadFile("day_11_input.txt")
+	// b, err := os.ReadFile("day_11_sample_input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	input := string(b)
+	lines := strings.Split(input, "\n")
+	lines = lines[0 : len(lines)-1]
+
+	adj := map[string][]string{}
+
+	for _, line := range lines {
+		parts := strings.Split(line, ":")
+		device := parts[0]
+		outputs := parts[1]
+		adj[device] = strings.Split(outputs, " ")[1:]
+	}
+
+	result := dfs(adj, map[string]bool{}, "you", "out")
+	fmt.Println(result)
+	// bfs(adj, "you")
+}
+
+func copy(input map[string]bool) map[string]bool {
+	result := map[string]bool{}
+	for k, v := range input {
+		result[k] = v
+	}
+	return result
+}
+
+func dfs(adj map[string][]string, visited map[string]bool, start string, goal string) int {
+	if start == goal {
+		return 1
+	}
+
+	neighbors := adj[start]
+	sum := 0
+	for _, n := range neighbors {
+		if visited[n] {
+			continue
+		}
+		visited[n] = true
+		subVisit := copy(visited)
+		sum += dfs(adj, subVisit, n, "out")
+	}
+
+	return sum
+}
+
+func bfs(adj map[string][]string, start string) {
+	queue := []string{start}
+	visited := map[string]bool{}
+	visited[start] = true
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		fmt.Println(current)
+
+		adjacents := adj[current]
+		for _, a := range adjacents {
+			if visited[a] {
+				continue
+			}
+			visited[a] = true
+			queue = append(queue, a)
+		}
+	}
+}
