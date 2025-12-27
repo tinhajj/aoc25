ae2b279df69bacd689abb6bcc5d9743be1b29cea
Thanoj Singh <thanoj.singh@gmail.com>
2025-12-27 10:06:27 -0500

    maybe closer

--- day11/part2/main.go
+++ day11/part2/main.go
@@ -4,9 +4,17 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
+type Memo struct {
+	HitsFft   bool
+	HitsDac   bool
+	WaysToEnd int
+}
+
 func main() {
+	start := time.Now()
 	b, err := os.ReadFile("day_11_input.txt")
 	// b, err := os.ReadFile("day_11_sample_input.txt")
 	if err != nil {
@@ -26,14 +34,28 @@ func main() {
 		adj[device] = strings.Split(outputs, " ")[1:]
 	}
 
-	result := dfs(adj, map[string]bool{}, "svr", "out")
+	result, _, _ := dfs(adj, map[string]bool{}, map[string]Memo{}, "svr", "out")
+	fmt.Println(time.Since(start))
 
 	fmt.Println(result)
 }
 
-func dfs(adj map[string][]string, visited map[string]bool, start string, goal string) int {
+func dfs(adj map[string][]string, visited map[string]bool, cache map[string]Memo, start string, goal string) (int, bool, bool) {
 	if start == goal {
-		return 1
+		if visited["fft"] && visited["dac"] {
+			return 1, true, true
+		}
+		return 0, false, false
+	}
+
+	memo, ok := cache[start]
+	if ok {
+		if !visited["fft"] && !memo.HitsFft {
+			return 0, memo.HitsFft, memo.HitsDac
+		}
+		if !visited["dac"] && !memo.HitsDac {
+			return 0, memo.HitsFft, memo.HitsDac
+		}
 	}
 
 	visited[start] = true
@@ -41,15 +63,20 @@ func dfs(adj map[string][]string, visited map[string]bool, start string, goal st
 	neighbors := adj[start]
 	sum := 0
 
+	hitsFft := visited["fft"]
+	hitsDac := visited["dac"]
+
 	for _, n := range neighbors {
 		if visited[n] {
 			continue
 		}
-		result := dfs(adj, visited, n, goal)
+		result, hitFft, hitDac := dfs(adj, visited, cache, n, goal)
+		hitsFft = hitsFft || hitFft
+		hitsDac = hitsDac || hitDac
 		sum += result
 	}
 
 	visited[start] = false
 
-	return sum
+	return sum, hitsFft, hitsDac
 }
