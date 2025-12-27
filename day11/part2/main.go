a551d0fd34bf0a5f2f9b81776b2d14c135c07450
Thanoj Singh <thanoj.singh@gmail.com>
2025-12-27 01:24:52 -0500

    remove some stuff

--- day11/part2/main.go
+++ day11/part2/main.go
@@ -26,62 +26,30 @@ func main() {
 		adj[device] = strings.Split(outputs, " ")[1:]
 	}
 
-	result := dfs(adj, map[string]bool{}, map[string]bool{}, "svr", "fft")
+	result := dfs(adj, map[string]bool{}, "svr", "out")
 
 	fmt.Println(result)
-	// bfs(adj, "you")
 }
 
-func copy(input map[string]bool) map[string]bool {
-	result := map[string]bool{}
-	for k, v := range input {
-		result[k] = v
-	}
-	return result
-}
-
-func dfs(adj map[string][]string, visited map[string]bool, skip map[string]bool, start string, goal string) int {
-	visited[start] = true
+func dfs(adj map[string][]string, visited map[string]bool, start string, goal string) int {
 	if start == goal {
-		fmt.Println("found")
 		return 1
-		if visited["fft"] && visited["dac"] {
-			return 1
-		}
-		return 0
 	}
+
+	visited[start] = true
 
 	neighbors := adj[start]
 	sum := 0
+
 	for _, n := range neighbors {
 		if visited[n] {
 			continue
 		}
-		result := dfs(adj, copy(visited), skip, n, goal)
+		result := dfs(adj, visited, n, goal)
 		sum += result
 	}
 
+	visited[start] = false
+
 	return sum
-}
-
-func bfs(adj map[string][]string, start string) {
-	queue := []string{start}
-	visited := map[string]bool{}
-	visited[start] = true
-
-	for len(queue) > 0 {
-		current := queue[0]
-		queue = queue[1:]
-
-		fmt.Println(current)
-
-		adjacents := adj[current]
-		for _, a := range adjacents {
-			if visited[a] {
-				continue
-			}
-			visited[a] = true
-			queue = append(queue, a)
-		}
-	}
 }
