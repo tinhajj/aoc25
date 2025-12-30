11ce4dfcf42747bed471ce8e53be5e7e574e78a0
Thanoj Singh <thanoj.singh@gmail.com>
2025-12-30 00:42:24 -0500

    day 11 part 2 much simpler than anything I came up, this problem should assume no cycles

--- day11/part2/main.go
+++ day11/part2/main.go
@@ -7,13 +7,10 @@ import (
 	"time"
 )
 
-type Memo struct {
-	Visited map[string]bool
-
-	WaysGoal int
-	WaysFft  int
-	WaysDac  int
-	WaysBoth int
+type Input struct {
+	Start string
+	IsFft bool
+	IsDac bool
 }
 
 func main() {
@@ -37,105 +34,34 @@ func main() {
 		adj[device] = strings.Split(outputs, " ")[1:]
 	}
 
-	visited := map[string]bool{}
-	cache := map[string]Memo{}
+	cache := map[Input]int{}
 
-	goalWays, fftWays, dacWays, bothWays, _ := dfs(adj, visited, cache, "svr", "out")
+	ways := dfs(adj, cache, false, false, "svr", "out")
 	fmt.Println(time.Since(start))
 
-	fmt.Println(goalWays, fftWays, dacWays, bothWays)
+	fmt.Println(ways)
 }
 
-var largest = 0
-
-func dfs(adj map[string][]string, visited map[string]bool, cache map[string]Memo, start string, goal string) (waysAny, waysFft, waysDac, waysBoth int, v map[string]bool) {
-	visits := map[string]bool{}
-	visits[start] = true
-
-	if start == goal {
-		return 1, 0, 0, 0, visits
+func dfs(adj map[string][]string, cache map[Input]int, isFft, isDac bool, start string, goal string) int {
+	memo, ok := cache[Input{Start: start, IsFft: isFft, IsDac: isDac}]
+	if ok {
+		return memo
 	}
 
-	visited[start] = true
+	if start == goal {
+		if isFft && isDac {
+			return 1
+		}
+	}
 
 	neighbors := adj[start]
 
-	var outerWaysAny, outerWaysFft, outerWaysDac, outerWaysBoth int
-
+	total := 0
 	for _, n := range neighbors {
-		if visited[n] {
-			panic("loop back, didn't expect")
-		}
-
-		memo, ok := cache[n]
-
-		var subVisits map[string]bool
-
-		if ok {
-			if visited["fft"] && visited["dac"] {
-				outerWaysAny += memo.WaysGoal
-				outerWaysFft += memo.WaysGoal
-				outerWaysDac += memo.WaysGoal
-				outerWaysBoth += memo.WaysGoal
-			}
-
-			if visited["fft"] {
-				outerWaysAny += memo.WaysGoal
-				outerWaysFft += memo.WaysGoal
-				outerWaysDac += memo.WaysDac
-				outerWaysBoth += memo.WaysDac
-			}
-
-			if visited["dac"] {
-				outerWaysAny += memo.WaysGoal
-				outerWaysDac += memo.WaysGoal
-				outerWaysFft += memo.WaysFft
-				outerWaysBoth += memo.WaysFft
-			}
-
-			if !visited["dac"] && !visited["fft"] {
-				outerWaysAny += memo.WaysGoal
-				outerWaysDac += memo.WaysDac
-				outerWaysFft += memo.WaysFft
-				outerWaysBoth += memo.WaysBoth
-			}
-		}
-
-		if !ok {
-			goalWays, fftWays, dacWays, bothWays, subVisits = dfs(adj, visited, cache, n, goal)
-
-			outerGoalWays += goalWays
-			outerFftWays += fftWays
-			outerDacWays += dacWays
-			outerBothWays += bothWays
-		}
-
-		for k, v := range subVisits {
-			visits[k] = v
-		}
+		total += dfs(adj, cache, isFft || n == "fft", isDac || n == "dac", n, goal)
 	}
 
-	visited[start] = false
+	cache[Input{Start: start, IsFft: isFft, IsDac: isDac}] = total
 
-	vf := false
-	vd := false
-	for _, n := range neighbors {
-		memo, ok := cache[n]
-		if ok {
-			vf = vf || memo.Visited["fft"]
-			vd = vd || memo.Visited["dac"]
-		}
-		for k, v := range memo.Visited {
-			visits[k] = v
-		}
-	}
-
-	cache[start] = Memo{
-		WaysGoal: outerGoalWays,
-		WaysFft:  outerFftWays,
-		WaysDac:  outerDacWays,
-		Visited:  visits,
-	}
-
-	return outerGoalWays, outerFftWays, outerDacWays, outerBothWays, visits
+	return total
 }
